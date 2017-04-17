5c09af1e3806689a2ad96603d837d0a2a8b8e3b5
kwh3884858 <kwh3884858@126.com>
2017-04-17 20:37:16 +0800

    init sommit

---
+++ READEME.md
@@ -0,0 +1,4 @@
+## Angular Manager
+
+A knowledge library created using angular.
+
---
+++ l10n/cn.js
@@ -0,0 +1,77 @@
+{
+
+    "header" : {
+    "navbar" : {
+        "UPLOAD" : "Upload",
+            "new" : {
+            "NEW" : "New",
+                "PROJECT" : "项目",
+                "TASK" : "任务",
+                "USER" : "用户",
+                "EMAIL" : "Email"
+        },
+        "NOTIFICATIONS" : "Notifications"
+    }
+},
+    "aside" : {
+    "nav" : {
+        "HEADER" : "Navigation",
+            "LIBRARY" : "知识库",
+            "NOTE" : "备忘录",
+            "SEARCH": "搜索",
+            "STATISTIC" : "统计",
+            "PROFILE": "个人资料",
+            "DASHBOARD" : "Dashboard",
+            "CALENDAR" : "日历",
+            "EMAIL" : "Email",
+            "WIDGETS" : "Widgets",
+            "components" : {
+            "COMPONENTS" : "Components",
+                "ui_kits" : {
+                "UI_KITS" : "UI Kits",
+                    "BUTTONS" : "按钮",
+                    "ICONS" : "图标",
+                    "GRID" : "响应式布局",
+                    "BOOTSTRAP" : "Bootstrap",
+                    "SORTABLE" : "Sortable",
+                    "PORTLET" : "Portlet",
+                    "TIMELINE" : "Timeline",
+                    "VECTOR_MAP" : "Vector Map"
+            },
+            "table" : {
+                "TABLE" : "Table",
+                    "TABLE_STATIC" : "Table static",
+                    "DATATABLE" : "Datatable",
+                    "FOOTABLE" : "Footable"
+            },
+            "form" : {
+                "FORM" : "Form",
+                    "FORM_ELEMENTS" : "Form elements",
+                    "FORM_VALIDATION" : "Form validation",
+                    "FORM_WIZARD" : "Form wizard"
+            },
+            "CHART" : "Chart",
+                "pages" : {
+                "PAGES" : "Pages",
+                    "PROFILE" : "Profile",
+                    "POST" : "Post",
+                    "SEARCH" : "Search",
+                    "INVOICE" : "Invoice",
+                    "LOCK_SCREEN" : "Lock screen",
+                    "SIGNIN" : "Signin",
+                    "SIGNUP" : "Signup",
+                    "FORGOT_PASSWORD" : "Forgot password",
+                    "404" : "404"
+            }
+        },
+        "your_stuff" : {
+            "YOUR_STUFF": "Your Stuff",
+                "PROFILE" : "Profile",
+                "DOCUMENTS" : "Documents"
+        }
+    },
+    "MILESTONE" : "Milestone",
+        "RELEASE" : "Release"
+}
+
+}
